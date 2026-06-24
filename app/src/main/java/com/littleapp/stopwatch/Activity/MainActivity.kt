664d6bc6d2d6e69969c27b78f1948ed43c1d5bfe
selimdawa <selimdawa@gmail.com>
2026-06-25 01:16:31 +0300

    viewBinding Add

--- app/src/main/java/com/littleapp/stopwatch/Activity/MainActivity.kt
+++ app/src/main/java/com/littleapp/stopwatch/Activity/MainActivity.kt
@@ -3,56 +3,58 @@ package com.littleapp.stopwatch.Activity
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.os.SystemClock
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.littleapp.stopwatch.R
 import com.littleapp.stopwatch.Unit.THEME
 import com.littleapp.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding: ActivityMainBinding? = null
-    var context: Context = this@MainActivity
+    private lateinit var binding: ActivityMainBinding
+    private val context: Context = this
     private var isResume = false
-    var handler: Handler? = null
-    var tMilliSec: Long = 0
-    var tStart: Long = 0
-    var tBuff: Long = 0
-    var tUpdate = 0L
-    var sec = 0
-    var min = 0
-    var milliSec = 0
+    private val handler = Handler(Looper.getMainLooper())
+    private var tMilliSec: Long = 0
+    private var tStart: Long = 0
+    private var tBuff: Long = 0
+    private var tUpdate = 0L
+    private var sec = 0
+    private var min = 0
+    private var milliSec = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         THEME.setThemeOfApp(context)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding!!.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        binding!!.toolbar.nameSpace.text = getString(R.string.stop_watch)
-        handler = Handler()
-        binding!!.btStart.setOnClickListener {
+        binding.toolbar.nameSpace.text = getString(R.string.stop_watch)
+
+        binding.btStart.setOnClickListener {
             if (!isResume) {
                 tStart = SystemClock.uptimeMillis()
-                handler!!.postDelayed(runnable, 0)
-                binding!!.chronometer.start()
+                handler.postDelayed(runnable, 0)
+                binding.chronometer.start()
                 isResume = true
-                binding!!.btStop.visibility = View.GONE
-                binding!!.btStart.setImageDrawable(resources.getDrawable(R.drawable.ic_pause))
+                binding.btStop.visibility = View.GONE
+                binding.btStart.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_pause))
             } else {
                 tBuff += tMilliSec
-                handler!!.removeCallbacks(runnable)
-                binding!!.chronometer.stop()
+                handler.removeCallbacks(runnable)
+                binding.chronometer.stop()
                 isResume = false
-                binding!!.btStop.visibility = View.VISIBLE
-                binding!!.btStart.setImageDrawable(resources.getDrawable(R.drawable.ic_play))
+                binding.btStop.visibility = View.VISIBLE
+                binding.btStart.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_play))
             }
         }
-        binding!!.btStop.setOnClickListener {
+
+        binding.btStop.setOnClickListener {
             if (!isResume) {
-                binding!!.btStart.setImageDrawable(resources.getDrawable(R.drawable.ic_play))
+                binding.btStart.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_play))
                 tMilliSec = 0L
                 tStart = 0L
                 tBuff = 0L
@@ -60,13 +62,13 @@ class MainActivity : AppCompatActivity() {
                 sec = 0
                 min = 0
                 milliSec = 0
-                binding!!.lastTimeDate.text = binding!!.chronometer.text.toString()
-                binding!!.chronometer.text = "00:00:00"
+                binding.lastTimeDate.text = binding.chronometer.text.toString()
+                binding.chronometer.text = getString(R.string._00_00_00)
             }
         }
     }
 
-    var runnable: Runnable = object : Runnable {
+    private val runnable: Runnable = object : Runnable {
         override fun run() {
             tMilliSec = SystemClock.uptimeMillis() - tStart
             tUpdate = tBuff + tMilliSec
@@ -74,9 +76,13 @@ class MainActivity : AppCompatActivity() {
             min = sec / 60
             sec %= 60
             milliSec = (tUpdate % 100).toInt()
-            binding!!.chronometer.text = (String.format("%02d", min) + ":"
-                    + String.format("%02d", sec) + ":" + String.format("%02d", milliSec))
-            handler!!.postDelayed(this, 60)
+            binding.chronometer.text = String.format(getString(R.string._02d_02d_02d), min, sec, milliSec)
+            handler.postDelayed(this, 60)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(runnable)
     }
 }
